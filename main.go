ba2015a140752264dd748aa3870ee85ab889297e
Tor Bratsberg <tor@bratsberg.net>
2022-02-13 19:24:51 +0100

    Kinda workinKinda workingg

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 )
 
-// Window size
-const ws = 400.
-
-// Circle size
-const cs = 10.
+const (
+	ws = 800. // Windows size
+	ps = 10.  // Player size
+)
 
 func check(err error) {
 	if err != nil {
@@ -24,136 +23,227 @@ func check(err error) {
 }
 
 type PlayerStats struct {
-	X float64
-	Y float64
+	X, Y float64
 }
 
-type ObstaclesData struct {
-	Radius float64
-	X      float64
-	Y      float64
+type ObstacleData struct {
+	Radius, X, Y float64
 }
 
-func makeWindow() *pixelgl.Window {
-	// Make new window
-	windowConfig := pixelgl.WindowConfig{
-		Title:  "Game",
-		Bounds: pixel.R(0, 0, ws, ws),
-		VSync:  true,
-	}
-	win, err := pixelgl.NewWindow(windowConfig)
-	check(err)
-	return win
+type Ray struct {
+	Pos         pixel.Vec
+	HasCollided bool
+	imd         *imdraw.IMDraw
+}
+
+type Game struct {
+	ObstaclesData []ObstacleData
+	Obstacles     []*imdraw.IMDraw
+	Player        *imdraw.IMDraw
+	PlayerStats   PlayerStats
+	Win           *pixelgl.Window
+	Rays          []Ray
+	RaySpeed      float64
+	GameOver      bool
 }
 
 // Check if the player is colliding with anything inside the obstacles slice
-func isCollision(obstacles []ObstaclesData, playerStats PlayerStats) bool {
-	for _, obstacle := range obstacles {
+func (g *Game) isObstacleCollision() bool {
+	for _, obstacle := range g.ObstaclesData {
 		// Get distance between the obstacle and player
 		distance := math.Sqrt(
-			math.Pow(obstacle.X-playerStats.X, 2) +
-				math.Pow(obstacle.Y-playerStats.Y, 2))
+			math.Pow(obstacle.X-g.PlayerStats.X, 2) +
+				math.Pow(obstacle.Y-g.PlayerStats.Y, 2))
+
 		// Check is the distance minus obstacle radiuses is less than zero
-		if distance-cs-obstacle.Radius <= 0 {
+		if distance-ps-obstacle.Radius <= 0 {
 			return true
 		}
 	}
+
 	return false
 }
 
-// Check if player is inside the bounds of the window
-func isInsideBounds(playerStats PlayerStats) bool {
-	// Top bottom
-	if playerStats.Y-cs <= 0 || playerStats.Y+cs >= ws {
-		return false
+// Check if ray is colliding with any of the obstacles
+func (g *Game) isRayCollision(ray Ray) bool {
+	for _, obstacle := range g.ObstaclesData {
+		distance := math.Sqrt(
+			math.Pow(obstacle.X-ray.Pos.X, 2) +
+				math.Pow(obstacle.Y-ray.Pos.Y, 2))
+
+		if distance-obstacle.Radius <= 0 {
+			return true
+		}
 	}
-	// Left right
-	if playerStats.X-cs <= 0 || playerStats.X+cs >= ws {
-		return false
-	}
-	return true
+
+	return false
 }
 
-func run() {
-	win := makeWindow()
+func (g *Game) isDead(ray Ray) {
+	distance := math.Sqrt(
+		math.Pow(g.PlayerStats.X-ray.Pos.X, 2) +
+			math.Pow(g.PlayerStats.Y-ray.Pos.Y, 2))
 
-	// Create obstacles and the obstaclesData slice
-	obstaclesData := []ObstaclesData{
-		{Radius: 100, X: 50, Y: 100},
-		{Radius: 150, X: 200, Y: ws},
+	if distance-ps <= 0 {
+		g.GameOver = true
 	}
-	obstacles := []*imdraw.IMDraw{
-		imdraw.New(nil),
-		imdraw.New(nil),
+}
+
+// Check if player is inside the bounds of the window
+func (g *Game) isInsideBounds() bool {
+	// Top, bottom, left, right
+	if g.PlayerStats.Y-ps <= 0 || g.PlayerStats.Y+ps >= ws ||
+		g.PlayerStats.X-ps <= 0 || g.PlayerStats.X+ps >= ws {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (g *Game) Run() {
+	// Draw our obstacles
+	for i, data := range g.ObstaclesData {
+		g.Obstacles[i].Push(pixel.V(data.X, data.Y))
+		g.Obstacles[i].Circle(data.Radius, 5)
+		g.Obstacles[i].Draw(g.Win)
 	}
 
-	// Initiate player
-	player := imdraw.New(nil)
-
-	playerStats := PlayerStats{
-		X: ws - 50,
-		Y: 50,
+	// Draw our rays
+	for i, ray := range g.Rays {
+		g.Rays[i].imd.Push(pixel.V(ray.Pos.X, ray.Pos.Y))
+		g.Rays[i].imd.Line(1)
+		g.Rays[i].imd.Draw(g.Win)
 	}
 
-	// Draw our rectangles
-	for i, data := range obstaclesData {
-		obstacles[i].Push(pixel.V(data.X, data.Y))
-		obstacles[i].Circle(data.Radius, 1)
-		obstacles[i].Draw(win)
-	}
-
-	wiho := func(key pixelgl.Button) bool {
-		if win.JustPressed(key) || win.Repeated(key) {
+	registerKeyPress := func(key pixelgl.Button) bool {
+		if g.Win.JustPressed(key) || g.Win.Repeated(key) {
 			return true
 		}
 		return false
 	}
 
-	for !win.Closed() {
-		if win.JustPressed(pixelgl.KeyEscape) {
+	g.Win.SetSmooth(true)
+
+	speed := 1.
+
+	for !g.Win.Closed() {
+		if registerKeyPress(pixelgl.KeyEscape) {
 			break
 		}
 
-		win.Clear(pixel.RGB(0, 0, 0))
+		if !g.GameOver {
+			g.Win.Clear(pixel.RGB(0, 0, 0))
 
-		// Redraw our rectangles
-		for i := range obstacles {
-			obstacles[i].Draw(win)
-		}
+			// Redraw our rectangles
+			for i := range g.Obstacles {
+				g.Obstacles[i].Draw(g.Win)
+			}
 
-		// Store prev position in case we moved out of bounds
-		prev := playerStats
+			// Redraw our rays
+			for i, ray := range g.Rays {
+				if ray.HasCollided == false {
+					g.Rays[i].Pos.X += g.RaySpeed
+					g.Rays[i].imd.Push(pixel.V(ray.Pos.X, ray.Pos.Y))
+					g.Rays[i].imd.Push(pixel.V(ray.Pos.X+g.RaySpeed, ray.Pos.Y))
+					g.Rays[i].imd.Line(1.0001) // The float fixed a weird issue
+					g.Rays[i].imd.Draw(g.Win)
 
-		// Movements keys
-		if wiho(pixelgl.KeyW) {
-			playerStats.Y += 10
-		}
-		if wiho(pixelgl.KeyS) {
-			playerStats.Y -= 10
-		}
-		if wiho(pixelgl.KeyA) {
-			playerStats.X -= 10
-		}
-		if wiho(pixelgl.KeyD) {
-			playerStats.X += 10
-		}
+					if g.isRayCollision(ray) {
+						g.Rays[i].HasCollided = true
+					}
+					g.isDead(ray)
+				} else {
+					g.Rays[i].imd.Line(1)
+					g.Rays[i].imd.Draw(g.Win)
+				}
+			}
 
-		// Check collisions
-		if isInsideBounds(playerStats) && !isCollision(obstaclesData, playerStats) {
-			player.Clear()
-			player.Push(pixel.V(playerStats.X, playerStats.Y))
-			player.Circle(cs, 1)
-			player.Draw(win)
-		} else {
-			// Reset to the previous position
-			playerStats = prev
-		}
+			// Store prev position in case we moved out of bounds
+			prev := g.PlayerStats
 
+			if registerKeyPress(pixelgl.KeyLeftSuper) {
+				if speed == 1. {
+					speed = 20.
+				} else {
+					speed = 1.
+				}
+			}
+
+			// Movements keys
+			if registerKeyPress(pixelgl.KeyW) {
+				g.PlayerStats.Y += speed
+			}
+			if registerKeyPress(pixelgl.KeyS) {
+				g.PlayerStats.Y -= speed
+			}
+			if registerKeyPress(pixelgl.KeyA) {
+				g.PlayerStats.X -= speed
+			}
+			if registerKeyPress(pixelgl.KeyD) {
+				g.PlayerStats.X += speed
+			}
+
+			// Check collisions
+			if g.isInsideBounds() && !g.isObstacleCollision() {
+				g.Player.Clear()
+				g.Player.Push(pixel.V(g.PlayerStats.X, g.PlayerStats.Y))
+				g.Player.Circle(ps, 1)
+				g.Player.Draw(g.Win)
+			} else {
+				// Reset to the previous position
+				g.PlayerStats = prev
+			}
+		}
 		// Update window
-		win.Update()
+		g.Win.Update()
 	}
 }
 
 func main() {
-	pixelgl.Run(run)
+	pixelgl.Run(func() {
+
+		// Create window
+		win, err := pixelgl.NewWindow(pixelgl.WindowConfig{
+			Title:  "Game",
+			Bounds: pixel.R(0, 0, ws, ws),
+			VSync:  true,
+		})
+		check(err)
+
+		// Create the rays
+		rays := []Ray{}
+		for i := 10.; i < ws; i += 20 {
+			rays = append(
+				rays,
+				Ray{
+					Pos:         pixel.V(0, i),
+					HasCollided: false,
+					imd:         imdraw.New(nil),
+				},
+			)
+		}
+
+		// Initiate game
+		game := Game{
+			ObstaclesData: []ObstacleData{
+				{Radius: 20, X: 200, Y: 100},
+				{Radius: 50, X: 200, Y: ws - 100},
+			},
+			Obstacles: []*imdraw.IMDraw{
+				imdraw.New(nil),
+				imdraw.New(nil),
+			},
+			Player: imdraw.New(nil),
+			PlayerStats: PlayerStats{
+				X: ws - 50,
+				Y: ws / 2,
+			},
+			Win:      win,
+			Rays:     rays,
+			RaySpeed: 2,
+			GameOver: false,
+		}
+
+		game.Run()
+	})
 }
